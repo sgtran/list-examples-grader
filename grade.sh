349aa9ee624bd52dfe35af520a7ef951b9aa8437
sgtran <65384574+sgtran@users.noreply.github.com>
2023-02-27 12:57:23 -0800

    d

--- grade.sh
+++ grade.sh
@@ -3,3 +3,16 @@ CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
 rm -rf student-submission
 git clone $1 student-submission
 echo 'Finished cloning'
+
+#cp student-submission/ListExamples.java ./
+
+cd student-submission
+
+if [[ -e ListExamples.java ]]
+    then
+    echo "ListExample File Found"
+    else 
+    echo "ListExample File not found"
+fi
+
+javac -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar ListExamples.java
